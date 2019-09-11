8522f18cbb5357bcc52986e27ab60798ab42397b
Chris Tram <ctram@protonmail.com>
2019-09-11 15:23:06 -0700

    render meal items within MealStats component

--- src/components/MealStats.js
+++ src/components/MealStats.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import MealItem from './MealItem';
+
 function MealStats(props) {
-  const { name } = props;
+  const { name, items } = props;
+
+  let domInner = null;
+
+  if (items) {
+    const domItems = items.map((item) => {
+      return <MealItem key={item.id} item={item} />;
+    });
+
+    domInner = (
+      <ul class="list-group list-group-flush">
+        {domItems}
+      </ul>
+    );
+  }
 
   return (
-    <div className="meal-stats">
-      <h2 className="text-capitalize">{name}</h2>
+    <div className="meal-stats card">
+      <h2 className="text-capitalize card-header">{name}</h2>
+      {domInner}
     </div>
   );
 }
