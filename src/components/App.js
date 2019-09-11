e79aeb669b4c19e1d97f60a93fdd1bc56fcc7324
Chris Tram <ctram@protonmail.com>
2019-09-11 15:23:33 -0700

    turn App component into class; adds meal data as state.

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,52 @@ import '../styles/App.css';
 
 import NavBar from './NavBar';
 import MealStats from './MealStats';
+import mockData from '../mock-data/days.json';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <div className="main-content p-3">
-        <MealStats name="breakfast" />
-        <MealStats name="lunch" />
-        <MealStats name="dinner" />
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: '',
+      breakfast: {},
+      lunch: {},
+      dinner: {},
+      snacks: [],
+      exercises: []
+    };
+  }
+
+  componentDidMount() {
+    const { date, foods: { breakfast, lunch, dinner, snacks } } = mockData;
+
+    this.setState({
+      date, breakfast, lunch, dinner, snacks
+    });
+  }
+
+  render() {
+    const { breakfast, lunch, dinner, snacks, date } = this.state;
+
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="main-content p-3">
+          <div className="py-3">
+            <MealStats name="breakfast" items={breakfast.items} />
+          </div>
+          <div className="py-3">
+            <MealStats name="lunch" items={lunch.items} />
+          </div>
+          <div className="py-3">
+            <MealStats name="dinner" items={dinner.items} />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
