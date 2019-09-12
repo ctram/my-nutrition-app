68f71c4b3e6ae57a3f6fb08d280e6773ef176d58
Chris Tram <ctram@protonmail.com>
2019-09-11 23:42:32 -0700

    revise App component: there is now only one modal that exists at a time, with one unique ID

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import '../styles/App.css';
 
 import NavBar from './NavBar';
 import MealStats from './MealStats';
-import ModalAddFood from './ModalAddFood';
 import ModalAddFoodToMeal from './ModalAddFoodToMeal';
 
 import mockData from '../mock-data/days.json';
@@ -22,18 +21,24 @@ class App extends React.Component {
       exercises: [],
       modalAddFoodVisible: false,
       modalAddExerciseVisible: false,
-      mealTypeToAddItemTo: null
+      mealTypeToAddItemTo: null,
+      modalToShow: null
     };
+
+    this.idModal = 'my-modal';
 
     this.idModalAddFood = 'modal-add-food-to-meal';
     this.idModalAddExercise = 'modal-add-exercise';
-    this.toggleModal = this.toggleModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   componentDidMount() {
     const { date, foods: { breakfast, lunch, dinner, snacks } } = mockData;
 
-    window.$(`#${this.idModalAddFood}`)
+
+
+    window.$(`#${this.idModal}`)
       .modal({
         backdrop: 'static',
         show: false,
@@ -45,45 +50,45 @@ class App extends React.Component {
     });
   }
 
-  toggleModal(type) {
-    let { modalAddFoodVisible, modalAddExerciseVisible } = this.state;
+  showModal(modalType) {
+    window.$(`#${this.idModal}`).modal(!!modalType ? 'show' : 'hide');
+    this.setState({ modalToShow: modalType });
+  }
 
-    // tap into jQuery to activate Bootstrap's modal
-    switch (type) {
-      case 'breakfast':
-      case 'lunch':
-      case 'dinner':
-      case 'snack':
-        modalAddFoodVisible = !modalAddFoodVisible;
-        window.$(`#${this.idModalAddFood}`).modal('toggle');
-        break;
-      case 'exercise':
-        modalAddExerciseVisible = !modalAddExerciseVisible;
-        window.$(`#${this.idModalAddExercise}`).modal('toggle');
-        break;
-      default:
-    }
-
-    this.setState({
-      modalAddFoodVisible, modalAddExerciseVisible, mealTypeToAddItemTo: type
-    });
+  closeModal() {
+    this.showModal(null);
   }
 
   render() {
-    const { breakfast, lunch, dinner, snacks, date, mealTypeToAddItemTo } = this.state;
+    const { breakfast, lunch, dinner, snacks, date, mealTypeToAddItemTo, modalToShow } = this.state;
+
+    const defaultModal = <ModalAddFoodToMeal
+                            date={date}
+                            mealType={mealTypeToAddItemTo}
+                            onClickClose={this.closeModal}
+                          />;
+
+    let domModal = null;
+
+    switch (modalToShow) {
+      case 'modalAddFoodToMeal':
+        domModal = defaultModal
+        break;
+      default:
+        domModal = defaultModal;
+    }
+
 
     return (
       <div className="App">
         <NavBar />
-        <ModalAddFoodToMeal
-          id={this.idModalAddFood}
-          date={date}
-          mealType={mealTypeToAddItemTo}
-          onClickClose={this.toggleModal}
-        />
+        {domModal}
         <div className="main-content p-3">
           <div className="py-3">
-            <MealStats name="breakfast" items={breakfast.items} onClickAddFood={this.toggleModal} />
+            <MealStats
+              name="breakfast"
+              items={breakfast.items}
+              onClickAddFood={this.showModal} />
           </div>
           <div className="py-3">
             <MealStats name="lunch" items={lunch.items} />
