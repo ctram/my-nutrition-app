19d7439da4a6c38d4016451643c417e036681914
Chris Tram <ctram@protonmail.com>
2019-09-12 00:57:44 -0700

    adds helper method to create the template for a day

---
+++ src/helpers/days.js
@@ -0,0 +1,16 @@
+export function newDayTemplate() {
+  return {
+    "foods": {
+      "breakfast": {
+        "items": []
+      },
+      "lunch": {
+        "items": []
+      },
+      "dinner": {
+        "items": []
+      },
+      "snacks": []
+    }
+  };
+}
