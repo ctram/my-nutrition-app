c8f1c14a2ec52245b2bc6b47ccfa69c39ea42427
Chris Tram <ctram@protonmail.com>
2019-09-12 16:28:01 -0700

    asdasd

--- src/components/App.js
+++ src/components/App.js
@@ -12,20 +12,52 @@ import foodTemplatesData from "../mock-data/food-templates.json";
 import "../styles/App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { foodTemplates: [] };
+
+    this.addFoodToLibrary = this.addFoodToLibrary.bind(this);
+  }
+
+  componentDidMount() {
+    Promise.resolve().then(() => {
+      this.setState({ foodTemplates: foodTemplatesData.foods });
+    });
+  }
+
+  addFoodToLibrary(food) {
+    const { foodTemplates } = this.state;
+    const newFood = { ...food, id: Math.random() };
+    foodTemplates.push(newFood);
+    this.setState({foodTemplates });
+  }
+
   render() {
+    const { foodTemplates } = this.state;
+
     return (
       <Router>
         <div className="App">
           <NavBar />
           <div className="main-content p-3">
             <Switch>
-            <Route path="/add-food-to-library" component={PageAddFoodToLibrary} />
-            <Route path="/" component={PageDay} />
+              <Route
+                path="/add-food-to-library"
+                render={() => (
+                  <PageAddFoodToLibrary
+                    onSubmitNewFood={this.addFoodToLibrary}
+                  />
+                )}
+              />
+              <Route
+                path="/"
+                render={() => <PageDay foodTemplates={foodTemplates} />}
+              />
             </Switch>
           </div>
         </div>
       </Router>
-
     );
   }
 }
