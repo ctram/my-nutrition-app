8cdf60041832c93278d00537c44c37d2c3776513
Chris Tram <ctram@protonmail.com>
2019-09-11 23:39:33 -0700

    turn Modal into a class component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,42 +1,57 @@
 import React from 'react';
 
-function Modal (props) {
-  const { buttons, title, children, id, onClickClose } = props;
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let domButtons = null;
-
-  if (buttons) {
-    domButtons = buttons.map((button, idx) => {
-      const { label, cssClass, onClick, disabled } = button;
-
-      return <button disabled={disabled} key={idx} onClick={onClick} className={`btn ${cssClass} col my-1 mx-2`} type="button">
-        {label}
-      </button>;
-    });
+    this.onClickClose = this.onClickClose.bind(this);
   }
 
-  return (
-    <div className="modal fade" id={id} tabIndex="-1">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
-            <button onClick={onClickClose} type="button" className="close">
-              <span>&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            {children}
-          </div>
-          <div className="modal-footer">
-            <div className="row">
-              {domButtons}
+  onClickClose() {
+    this.props.onClickClose();
+  }
+
+  render() {
+    const { buttons, title, children, id, onClickClose } = this.props;
+
+
+
+    let domButtons = null;
+
+    if (buttons) {
+      domButtons = buttons.map((button, idx) => {
+        const { label, cssClass, onClick, disabled } = button;
+
+        return <button disabled={disabled} key={idx} onClick={onClick} className={`btn ${cssClass} col my-1 mx-2`} type="button">
+          {label}
+        </button>;
+      });
+    }
+
+    return (
+      <div className="modal fade" id="my-modal" tabIndex="-1">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{title}</h5>
+              <button onClick={this.onClickClose} type="button" className="close">
+                <span>&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              {children}
+            </div>
+            <div className="modal-footer">
+              <div className="row">
+                {domButtons}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default Modal;
