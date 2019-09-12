ed290cdfa1e16cb5324f9b74923f18d5b0dd7a34
Chris Tram <ctram@protonmail.com>
2019-09-11 21:14:34 -0700

    adds more props for Modal: onClickClose; revise variable name for the label of a button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
 function Modal (props) {
-  const { buttons, title, children } = props;
+  const { buttons, title, children, id, onClickClose } = props;
 
   let domButtons = null;
 
   if (buttons) {
     domButtons = buttons.map((button, idx) => {
-      const { text, cssClass, onClick } = button;
+      const { label, cssClass, onClick } = button;
 
-      return <button onClick={onClick} className={`btn ${cssClass}`} type="button">
-        {text}
+      return <button key={idx} onClick={onClick} className={`btn ${cssClass}`} type="button">
+        {label}
       </button>;
     });
   }
 
   return (
-    <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog" role="document">
+    <div className="modal fade" id={id} tabIndex="-1">
+      <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">{title}</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
+            <h5 className="modal-title">{title}</h5>
+            <button onClick={onClickClose} type="button" className="close">
+              <span>&times;</span>
             </button>
           </div>
           <div className="modal-body">
