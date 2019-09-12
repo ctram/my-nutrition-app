bed1c510502c19ced37142458d8af48973d9cf70
Chris Tram <ctram@protonmail.com>
2019-09-11 17:01:38 -0700

    Add button to add food to a meal.

--- src/components/MealStats.js
+++ src/components/MealStats.js
@@ -6,60 +6,77 @@ import MealItem from './MealItem';
 
 import { sumItemStats } from '../helpers/calculations';
 
-function MealStats(props) {
-  const { name, items } = props;
+class MealStats extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let domItems = null;
-  let domTotalMealStats = null;
+    this.onClickAddFood = this.onClickAddFood.bind(this);
+  }
 
-  if (items) {
-    domItems = items.map((item) => {
-      return <MealItem key={item.id} item={item} />;
-    });
+  onClickAddFood(e) {
+    // open modal to add food.
+  }
 
-    domItems = (
-      <ul class="list-group list-group-flush">
-        {domItems}
-      </ul>
-    );
+  render() {
+    const { name, items } = this.props;
 
-    // we can also do this summation in the map() above, but let's keep
-    // code sepearate for ease of reading.
-    const { fat, protein, carbs, calories } = sumItemStats(items);
+    let domItems = null;
+    let domTotalMealStats = null;
 
-    domTotalMealStats = (
-      <div className="meal-stats-summary">
-        <div className="d-flex justify-content-between">
-          <div>
-            <span>Fat: </span>
-            <span>{fat}</span>
+    if (items) {
+      domItems = items.map((item) => {
+        return <MealItem key={item.id} item={item} />;
+      });
+
+      domItems = (
+        <ul class="list-group list-group-flush">
+          {domItems}
+        </ul>
+      );
+
+      // we can also do this summation in the map() above, but let's keep
+      // code sepearate for ease of reading.
+      const { fat, protein, carbs, calories } = sumItemStats(items);
+
+      domTotalMealStats = (
+        <div className="meal-stats-summary">
+          <div className="d-flex justify-content-between">
+            <div>
+              <span>Fat: </span>
+              <span>{fat}</span>
+            </div>
+            <div>
+              <span>Protein: </span>
+              <span>{protein}</span>
+            </div>
+            <div>
+              <span>Carbs: </span>
+              <span>{carbs}</span>
+            </div>
           </div>
           <div>
-            <span>Protein: </span>
-            <span>{protein}</span>
-          </div>
-          <div>
-            <span>Carbs: </span>
-            <span>{carbs}</span>
+            <span>Calories: </span>
+            <span>{calories}</span>
           </div>
         </div>
-        <div>
-          <span>Calories: </span>
-          <span>{calories}</span>
+      );
+    }
+
+    return (
+      <div className="meal-stats card">
+        <div className="card-body">
+          <h2 className="card-title text-capitalize">{name}</h2>
+          {domTotalMealStats}
+        </div>
+        {domItems}
+        <div class="card-footer text-muted">
+          <button data-toggle="modal" data-target="#exampleModal" className="btn btn-primary" type="button">
+            Add Food
+          </button>
         </div>
       </div>
     );
   }
-
-  return (
-    <div className="meal-stats card">
-      <div className="card-body">
-        <h2 className="card-title text-capitalize">{name}</h2>
-        {domTotalMealStats}
-      </div>
-      {domItems}
-    </div>
-  );
 }
 
 export default MealStats;
