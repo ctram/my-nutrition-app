cd998f7e845cfae4760020e32bc34cfd25d2062c
Chris Tram <ctram@protonmail.com>
2019-09-12 15:23:45 -0700

    adds more controls for add-food-to-library- form

--- src/components/ModalAddFoodToLibrary.js
+++ src/components/ModalAddFoodToLibrary.js
@@ -22,7 +22,36 @@ class ModalAddFoodToLibrary extends React.Component {
         <form id={formId}>
           <div className="form-group">
             <label htmlFor="input-food-name">Name</label>
-            <input type="text" className="form-control" />
+            <input id="input-food-name" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="select-food-serving-unit">Serving Unit</label>
+            <select id="select-food-serving-unit">
+              <option value="pound">Pound</option>
+              <option value="ounce">Ounce</option>
+              <option value="gram">Gram</option>
+              <option value="liter">Liter</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-food-serving-size">Serving Size</label>
+            <input id="input-food-serving-size" type="number" min="1" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-fat">Fat (g)</label>
+            <input id="input-fat" type="number" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-protein">Proten (g)</label>
+            <input id="input-protein" type="number" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-carbs">Carbs (g)</label>
+            <input id="input-carbs" type="number" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="input-calories">Calories (g)</label>
+            <input id="input-calories" type="number" className="form-control" />
           </div>
         </form>
       </Modal>
