8b1e8793881830b66ff7bff7992bdd9e1a560986
Chris Tram <ctram@protonmail.com>
2019-09-13 15:26:24 -0700

    correct name of component class -- pass initial attribute values to generic form

--- src/pages/PageAddExerciseToLibrary.js
+++ src/pages/PageAddExerciseToLibrary.js
@@ -4,9 +4,13 @@ import FormAddGenericItemToLibrary from '../components/FormAddGenericItemToLibra
 
 import { Link } from "react-router-dom";
 
-class PageAddFoodToLibrary extends React.Component {
+class PageAddExerciseToLibrary extends React.Component {
   render() {
-    const { onSubmitNewExercise } = this.props;
+    const { onSubmitExerciseTemplate } = this.props;
+
+    const dataAttributes = {
+      name: ''
+    };
 
     return (
       <div className="page-add-food-to-library">
@@ -16,7 +20,10 @@ class PageAddFoodToLibrary extends React.Component {
 
         <div className="my-5">
           <h1 className="mb-5">Add Exercise To Library</h1>
-          <FormAddGenericItemToLibrary onSubmit={onSubmitNewExercise}>
+          <FormAddGenericItemToLibrary
+            onSubmit={onSubmitExerciseTemplate}
+            dataAttributes={dataAttributes}
+          >
             <div className="form-group">
               <label htmlFor="input-name">Name</label>
               <input data-attr-name="name" id="input-name" type="text" className="form-control" />
@@ -28,4 +35,4 @@ class PageAddFoodToLibrary extends React.Component {
   }
 }
 
-export default PageAddFoodToLibrary;
+export default PageAddExerciseToLibrary;
