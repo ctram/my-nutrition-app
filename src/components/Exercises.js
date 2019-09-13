84f593b015025d659794cceaede76f3f4006e696
Chris Tram <ctram@protonmail.com>
2019-09-13 00:31:53 -0700

    adds component for displaying exercises performed in the day

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 class Exercises extends React.Component {
   render() {
+    const { onClickAddExercise } = this.props;
+
     return (
       <div className="exercises card">
         <div className="card-body">
           <h2 className="card-title text-capitalize">Exercises</h2>
         </div>
         <div className="card-footer text-muted">
-          <button onClick={this.onClickAddExercise} className="btn btn-primary" type="button">
+          <button onClick={onClickAddExercise} className="btn btn-primary" type="button">
             Add Exercise
           </button>
         </div>
