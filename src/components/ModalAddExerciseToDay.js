49c50a15f58c2821abef0e873b8fac40e2e007d5
Chris Tram <ctram@protonmail.com>
2019-09-14 14:17:41 -0700

    DRY up code; let both ModalAddExerciseToDay and ModalAddFoodToMeal use ModalItem component which handles keeping track states and submission.

--- src/components/ModalAddExerciseToDay.js
+++ src/components/ModalAddExerciseToDay.js
@@ -1,140 +1,89 @@
 import React from "react";
 
-import Modal from "./Modal";
-import FormAddGenericItem from "./FormAddGenericItem";
+import ModalAddItem from "./ModalAddItem";
 
-class ModalAddExerciseToDay extends React.Component {
-  constructor(props) {
-    super(props);
+function ModalAddExerciseToDay(props) {
+  const { exerciseTemplates, onClickClose, onSubmitExerciseToDay } = props;
 
-    this.state = {
-      disableSaveButton: true
-    };
+  let domOptions = [<option key={-1} value={""} label="Select An Exercise" />];
 
-    this.handleChange = this.handleChange.bind(this);
+  if (exerciseTemplates && exerciseTemplates.length > 0) {
+    const _exerciseTemplates = exerciseTemplates.map(exerciseTemplate => {
+      const { name, id } = exerciseTemplate;
+
+      return <option key={id} value={id} label={name} />;
+    });
+
+    domOptions = domOptions.concat(_exerciseTemplates);
   }
 
-  handleChange(e) {
-    const target = e.target;
-    let { disableSaveButton } = this.state;
+  const dataAttributeIds = {
+    id: "select-exercise-id",
+    repsPerSet: "input-reps-per-set",
+    numberOfSets: "input-number-of-sets",
+    duration: "input-duration"
+  };
 
-    if (target.getAttribute("data-attr-name") === "id") {
-      disableSaveButton = target.value === "";
-      this.setState({ disableSaveButton });
-    }
-  }
-
-  render() {
-    const {
-      exerciseTemplates,
-      onClickClose,
-      onSubmitExerciseToDay
-    } = this.props;
-    const { disableSaveButton } = this.state;
-
-    const formId = "form-add-exercise-to-day";
-
-    const buttons = [
-      {
-        formId,
-        label: "Add Exercise To Day",
-        cssClass: "btn-primary",
-        type: "submit",
-        disabled: disableSaveButton
-      }
-    ];
-
-    let domOptions = [
-      <option key={-1} value={""} label="Select An Exercise" />
-    ];
-
-    if (exerciseTemplates && exerciseTemplates.length > 0) {
-      const _exerciseTemplates = exerciseTemplates.map(exerciseTemplate => {
-        const { name, id } = exerciseTemplate;
-
-        return <option key={id} value={id} label={name} />;
-      });
-
-      domOptions = domOptions.concat(_exerciseTemplates);
-    }
-
-    const dataAttributeIds = {
-      id: "select-exercise-id",
-      repsPerSet: "input-reps-per-set",
-      numberOfSets: "input-number-of-sets",
-      duration: "input-duration"
-    };
-
-    return (
-      <Modal
-        className="modal-add-exercise-to-day"
-        buttons={buttons}
-        title="Add Exercise To Day"
-        onClickClose={onClickClose}
-      >
-        <FormAddGenericItem
-          formId={formId}
-          dataAttributeIds={dataAttributeIds}
-          onSubmit={onSubmitExerciseToDay}
-          saveButtonVisible={false}
-          onChange={this.handleChange}
+  return (
+    <ModalAddItem
+      onClickClose={onClickClose}
+      modalTitle="Add Exercise To Day"
+      modalClassName="modal-add-exercise-to-day"
+      onSubmit={onSubmitExerciseToDay}
+      formId="form-add-exercise-to-day"
+      submitButtonLabel="Add Exercise To Day"
+      dataAttributeIds={dataAttributeIds}
+    >
+      <div className="form-group">
+        <label htmlFor={dataAttributeIds.id}>Exercise Name</label>
+        <select
+          data-attr-name="id"
+          defaultValue=""
+          id={dataAttributeIds.id}
+          className="form-control text-capitalize"
+          required
         >
-          <div className="form-group">
-            <label htmlFor={dataAttributeIds.id}>Exercise Name</label>
-            <select
-              data-attr-name="id"
-              defaultValue=""
-              id={dataAttributeIds.id}
-              className="form-control text-capitalize"
-              required
-            >
-              {domOptions}
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor={dataAttributeIds.duration}>
-              Duration (minutes)
-            </label>
-            <input
-              defaultValue="0"
-              data-attr-name="duration"
-              id={dataAttributeIds.duration}
-              type="number"
-              min="0"
-              className="form-control"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor={dataAttributeIds.repsPerSet}>Reps Per Set</label>
-            <input
-              defaultValue="0"
-              data-attr-name="repsPerSet"
-              id={dataAttributeIds.repsPerSet}
-              type="number"
-              min="0"
-              className="form-control"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor={dataAttributeIds.numberOfSets}>
-              Number Of Sets
-            </label>
-            <input
-              defaultValue="0"
-              data-attr-name="numberOfSets"
-              id={dataAttributeIds.numberOfSets}
-              type="number"
-              min="0"
-              className="form-control"
-              required
-            />
-          </div>
-        </FormAddGenericItem>
-      </Modal>
-    );
-  }
+          {domOptions}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor={dataAttributeIds.duration}>Duration (minutes)</label>
+        <input
+          defaultValue="0"
+          data-attr-name="duration"
+          id={dataAttributeIds.duration}
+          type="number"
+          min="0"
+          className="form-control"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor={dataAttributeIds.repsPerSet}>Reps Per Set</label>
+        <input
+          defaultValue="0"
+          data-attr-name="repsPerSet"
+          id={dataAttributeIds.repsPerSet}
+          type="number"
+          min="0"
+          className="form-control"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor={dataAttributeIds.numberOfSets}>Number Of Sets</label>
+        <input
+          defaultValue="0"
+          data-attr-name="numberOfSets"
+          id={dataAttributeIds.numberOfSets}
+          type="number"
+          min="0"
+          className="form-control"
+          required
+        />
+      </div>
+    </ModalAddItem>
+  );
 }
 
 export default ModalAddExerciseToDay;
--- src/components/ModalAddFoodToMeal.js
+++ src/components/ModalAddFoodToMeal.js
@@ -1,110 +1,68 @@
 import React from "react";
 
-import Modal from "./Modal";
-import FormAddGenericItem from "./FormAddGenericItem";
+import ModalAddItem from "./ModalAddItem";
 
-class ModalAddFoodToMeal extends React.Component {
-  constructor(props) {
-    super(props);
+function ModalAddFoodToMeal(props) {
+  const { onClickClose, foodTemplates, onSubmitFoodToMeal } = props;
 
-    this.state = {
-      disableSaveButton: true
-    };
+  let domOptions = [<option key={-1} value={""} label="Select A Food" />];
 
-    this.handleChange = this.handleChange.bind(this);
+  if (foodTemplates && foodTemplates.length > 0) {
+    const _foodTemplates = foodTemplates.map((foodTemplate, idx) => {
+      const { name, servingSize, servingUnit, id } = foodTemplate;
+
+      return (
+        <option
+          key={idx}
+          value={id}
+          label={`${name} (${servingSize} ${servingUnit})`}
+        />
+      );
+    });
+
+    domOptions = domOptions.concat(_foodTemplates);
   }
 
-  handleChange(e) {
-    const target = e.target;
-    let { disableSaveButton } = this.state;
+  const dataAttributeIds = {
+    id: "select-food-id",
+    numberServings: "input-number-servings"
+  };
 
-    if (target.getAttribute("data-attr-name") === "id") {
-      disableSaveButton = target.value === "";
-      this.setState({ disableSaveButton });
-    }
-  }
-
-  render() {
-    const { onClickClose, foodTemplates, onSubmitFoodToMeal } = this.props;
-    const { disableSaveButton } = this.state;
-
-    const formId = "form-add-food-to-meal";
-
-    const buttons = [
-      {
-        formId,
-        label: "Add Food To Meal",
-        cssClass: "btn-primary",
-        type: "submit",
-        disabled: disableSaveButton
-      }
-    ];
-
-    let domOptions = [<option key={-1} value={""} label="Select A Food" />];
-
-    if (foodTemplates && foodTemplates.length > 0) {
-      const _foodTemplates = foodTemplates.map((foodTemplate, idx) => {
-        const { name, servingSize, servingUnit, id } = foodTemplate;
-
-        return (
-          <option
-            key={idx}
-            value={id}
-            label={`${name} (${servingSize} ${servingUnit})`}
-          />
-        );
-      });
-
-      domOptions = domOptions.concat(_foodTemplates);
-    }
-
-    const dataAttributeIds = {
-      id: "select-food-id",
-      numberServings: "input-number-servings"
-    };
-
-    return (
-      <Modal
-        className="modal-add-food-to-meal"
-        buttons={buttons}
-        title="Add Food To Meal"
-        onClickClose={onClickClose}
-      >
-        <FormAddGenericItem
-          formId={formId}
-          dataAttributeIds={dataAttributeIds}
-          onSubmit={onSubmitFoodToMeal}
-          saveButtonVisible={false}
-          onChange={this.handleChange}
+  return (
+    <ModalAddItem
+      onClickClose={onClickClose}
+      submitButtonLabel="Add Food To Meal"
+      formId="form-add-food-to-meal"
+      dataAttributeIds={dataAttributeIds}
+      onSubmit={onSubmitFoodToMeal}
+      modalTitle="Add Food To Meal"
+    >
+      <div className="form-group">
+        <label htmlFor="select-food">Food Name</label>
+        <select
+          data-attr-name="id"
+          defaultValue=""
+          id={dataAttributeIds.id}
+          className="form-control text-capitalize"
+          required
         >
-          <div className="form-group">
-            <label htmlFor="select-food">Food Name</label>
-            <select
-              data-attr-name="id"
-              defaultValue=""
-              id={dataAttributeIds.id}
-              className="form-control text-capitalize"
-              required
-            >
-              {domOptions}
-            </select>
-          </div>
+          {domOptions}
+        </select>
+      </div>
 
-          <div className="form-group">
-            <label htmlFor="input-number-servings">Number of Servings</label>
-            <input
-              data-attr-name="numberServings"
-              defaultValue="1"
-              min="1"
-              id={dataAttributeIds.numberServings}
-              type="number"
-              className="form-control"
-            />
-          </div>
-        </FormAddGenericItem>
-      </Modal>
-    );
-  }
+      <div className="form-group">
+        <label htmlFor="input-number-servings">Number of Servings</label>
+        <input
+          data-attr-name="numberServings"
+          defaultValue="1"
+          min="1"
+          id={dataAttributeIds.numberServings}
+          type="number"
+          className="form-control"
+        />
+      </div>
+    </ModalAddItem>
+  );
 }
 
 export default ModalAddFoodToMeal;
