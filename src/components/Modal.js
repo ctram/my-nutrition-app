8e48637e6bbe4b2c7eec36798e261b74614b69e8
Chris Tram <ctram@protonmail.com>
2019-09-14 15:20:15 -0700

    transform component class to function

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,50 +1,48 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-class Modal extends React.Component {
-  render() {
-    const { buttons, title, children, onClickClose } = this.props;
+function Modal(props) {
+  const { buttons, title, children, onClickClose } = props;
 
-    let domButtons = null;
+  let domButtons = null;
 
-    if (buttons) {
-      domButtons = buttons.map((button, idx) => {
-        const { label, cssClass, onClick, disabled, formId, type } = button;
+  if (buttons) {
+    domButtons = buttons.map((button, idx) => {
+      const { label, cssClass, onClick, disabled, formId, type } = button;
 
-        return (
-          <button
-            form={formId}
-            disabled={disabled}
-            key={idx}
-            onClick={onClick}
-            className={`btn ${cssClass} col my-1 mx-2`}
-            type={type}
-          >
-            {label}
-          </button>
-        );
-      });
-    }
+      return (
+        <button
+          form={formId}
+          disabled={disabled}
+          key={idx}
+          onClick={onClick}
+          className={`btn ${cssClass} col my-1 mx-2`}
+          type={type}
+        >
+          {label}
+        </button>
+      );
+    });
+  }
 
-    return (
-      <div className="modal fade" id="my-modal" tabIndex="-1">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">{title}</h5>
-              <button onClick={onClickClose} type="button" className="close">
-                <span>&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">{children}</div>
-            <div className="modal-footer">
-              <div className="row">{domButtons}</div>
-            </div>
+  return (
+    <div className="modal fade" id="my-modal" tabIndex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button onClick={onClickClose} type="button" className="close">
+              <span>&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <div className="row">{domButtons}</div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Modal.propTypes = {
