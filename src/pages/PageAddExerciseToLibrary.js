8be8a951c34fcc594714f93432315b56de954d54
Chris Tram <ctram@protonmail.com>
2019-09-14 15:29:25 -0700

    convert class to functional component

--- src/pages/PageAddExerciseToLibrary.js
+++ src/pages/PageAddExerciseToLibrary.js
@@ -4,40 +4,38 @@ import FormAddGenericItem from "../components/FormAddGenericItem";
 
 import { Link } from "react-router-dom";
 
-class PageAddExerciseToLibrary extends React.Component {
-  render() {
-    const { onSubmitExerciseTemplate } = this.props;
+function PageAddExerciseToLibrary(props) {
+  const { onSubmitExerciseTemplate } = props;
 
-    const dataAttributeIds = {
-      name: "input-exercise-name"
-    };
+  const dataAttributeIds = {
+    name: "input-exercise-name"
+  };
 
-    return (
-      <div className="page-add-food-to-library page-responsive-width">
-        <Link to="/" className="btn btn-secondary">
-          Back To Diary
-        </Link>
+  return (
+    <div className="page-add-food-to-library page-responsive-width">
+      <Link to="/" className="btn btn-secondary">
+        Back To Diary
+      </Link>
 
-        <div className="my-5">
-          <h1 className="mb-5">Add Exercise To Library</h1>
-          <FormAddGenericItem
-            onSubmit={onSubmitExerciseTemplate}
-            dataAttributeIds={dataAttributeIds}
-          >
-            <div className="form-group">
-              <label htmlFor="input-name">Name</label>
-              <input
-                data-attr-name="name"
-                id={dataAttributeIds.name}
-                type="text"
-                className="form-control"
-              />
-            </div>
-          </FormAddGenericItem>
-        </div>
+      <div className="my-5">
+        <h1 className="mb-5">Add Exercise To Library</h1>
+        <FormAddGenericItem
+          onSubmit={onSubmitExerciseTemplate}
+          dataAttributeIds={dataAttributeIds}
+        >
+          <div className="form-group">
+            <label htmlFor="input-name">Name</label>
+            <input
+              data-attr-name="name"
+              id={dataAttributeIds.name}
+              type="text"
+              className="form-control"
+            />
+          </div>
+        </FormAddGenericItem>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default PageAddExerciseToLibrary;
--- src/pages/PageAddFoodToLibrary.js
+++ src/pages/PageAddFoodToLibrary.js
@@ -6,140 +6,138 @@ import { Link } from "react-router-dom";
 
 import { MEASURING_UNITS_LABELS } from "../constants/constants";
 
-class PageAddFoodToLibrary extends React.Component {
-  render() {
-    const { onSubmitFoodTemplate } = this.props;
-    const formId = "form-add-food-to-library";
+function PageAddFoodToLibrary(props) {
+  const { onSubmitFoodTemplate } = props;
+  const formId = "form-add-food-to-library";
 
-    const domServingUnitOptions = [
-      <option key={-1} value="">
-        Select A Unit
+  const domServingUnitOptions = [
+    <option key={-1} value="">
+      Select A Unit
+    </option>
+  ];
+
+  for (const fullLabel in MEASURING_UNITS_LABELS) {
+    domServingUnitOptions.push(
+      <option key={fullLabel} value={fullLabel}>
+        {fullLabel}
       </option>
-    ];
-
-    for (const fullLabel in MEASURING_UNITS_LABELS) {
-      domServingUnitOptions.push(
-        <option key={fullLabel} value={fullLabel}>
-          {fullLabel}
-        </option>
-      );
-    }
-
-    const dataAttributeIds = {
-      name: "input-food-name",
-      servingSize: "input-food-serving-size",
-      servingUnit: "select-food-serving-unit",
-      fat: "input-food-fat",
-      carbs: "input-food-carbs",
-      protein: "input-food-protein",
-      calories: "input-food-calories"
-    };
-
-    return (
-      <div className="page-add-food-to-library page-responsive-width">
-        <Link to="/" className="btn btn-secondary">
-          Back To Diary
-        </Link>
-
-        <div className="my-5">
-          <h1 className="mb-5">Add Food To Library</h1>
-          <FormAddGenericItem
-            formId={formId}
-            onSubmit={onSubmitFoodTemplate}
-            dataAttributeIds={dataAttributeIds}
-          >
-            <div className="form-group">
-              <label htmlFor={dataAttributeIds.name}>Name</label>
-              <input
-                defaultValue=""
-                id={dataAttributeIds.name}
-                data-attr-name="name"
-                type="text"
-                className="form-control"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor={dataAttributeIds.servingUnit}>Serving Unit</label>
-              <select
-                defaultValue=""
-                id={dataAttributeIds.servingUnit}
-                data-attr-name="servingUnit"
-                className="form-control text-capitalize"
-                required
-              >
-                {domServingUnitOptions}
-              </select>
-            </div>
-            <div className="form-group">
-              <label htmlFor={dataAttributeIds.servingSize}>Serving Size</label>
-              <input
-                defaultValue="1"
-                id={dataAttributeIds.servingSize}
-                data-attr-name="servingSize"
-                type="number"
-                min="1"
-                className="form-control"
-                required
-              />
-            </div>
-
-            <fieldset>
-              <legend>Nutrition Per Serving</legend>
-              <div className="form-group">
-                <label htmlFor={dataAttributeIds.fat}>Fat (g)</label>
-                <input
-                  defaultValue="0"
-                  id={dataAttributeIds.fat}
-                  data-attr-name="fat"
-                  type="number"
-                  min="0"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor={dataAttributeIds.protein}>Protein (g)</label>
-                <input
-                  defaultValue="0"
-                  id={dataAttributeIds.protein}
-                  data-attr-name="protein"
-                  type="number"
-                  min="0"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor={dataAttributeIds.carbs}>Carbs (g)</label>
-                <input
-                  defaultValue="0"
-                  id={dataAttributeIds.carbs}
-                  data-attr-name="carbs"
-                  type="number"
-                  min="0"
-                  className="form-control"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor={dataAttributeIds.calories}>Calories (g)</label>
-                <input
-                  defaultValue="0"
-                  id={dataAttributeIds.calories}
-                  data-attr-name="calories"
-                  type="number"
-                  min="0"
-                  className="form-control"
-                  required
-                />
-              </div>
-            </fieldset>
-          </FormAddGenericItem>
-        </div>
-      </div>
     );
   }
+
+  const dataAttributeIds = {
+    name: "input-food-name",
+    servingSize: "input-food-serving-size",
+    servingUnit: "select-food-serving-unit",
+    fat: "input-food-fat",
+    carbs: "input-food-carbs",
+    protein: "input-food-protein",
+    calories: "input-food-calories"
+  };
+
+  return (
+    <div className="page-add-food-to-library page-responsive-width">
+      <Link to="/" className="btn btn-secondary">
+        Back To Diary
+      </Link>
+
+      <div className="my-5">
+        <h1 className="mb-5">Add Food To Library</h1>
+        <FormAddGenericItem
+          formId={formId}
+          onSubmit={onSubmitFoodTemplate}
+          dataAttributeIds={dataAttributeIds}
+        >
+          <div className="form-group">
+            <label htmlFor={dataAttributeIds.name}>Name</label>
+            <input
+              defaultValue=""
+              id={dataAttributeIds.name}
+              data-attr-name="name"
+              type="text"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor={dataAttributeIds.servingUnit}>Serving Unit</label>
+            <select
+              defaultValue=""
+              id={dataAttributeIds.servingUnit}
+              data-attr-name="servingUnit"
+              className="form-control text-capitalize"
+              required
+            >
+              {domServingUnitOptions}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor={dataAttributeIds.servingSize}>Serving Size</label>
+            <input
+              defaultValue="1"
+              id={dataAttributeIds.servingSize}
+              data-attr-name="servingSize"
+              type="number"
+              min="1"
+              className="form-control"
+              required
+            />
+          </div>
+
+          <fieldset>
+            <legend>Nutrition Per Serving</legend>
+            <div className="form-group">
+              <label htmlFor={dataAttributeIds.fat}>Fat (g)</label>
+              <input
+                defaultValue="0"
+                id={dataAttributeIds.fat}
+                data-attr-name="fat"
+                type="number"
+                min="0"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor={dataAttributeIds.protein}>Protein (g)</label>
+              <input
+                defaultValue="0"
+                id={dataAttributeIds.protein}
+                data-attr-name="protein"
+                type="number"
+                min="0"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor={dataAttributeIds.carbs}>Carbs (g)</label>
+              <input
+                defaultValue="0"
+                id={dataAttributeIds.carbs}
+                data-attr-name="carbs"
+                type="number"
+                min="0"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor={dataAttributeIds.calories}>Calories (g)</label>
+              <input
+                defaultValue="0"
+                id={dataAttributeIds.calories}
+                data-attr-name="calories"
+                type="number"
+                min="0"
+                className="form-control"
+                required
+              />
+            </div>
+          </fieldset>
+        </FormAddGenericItem>
+      </div>
+    </div>
+  );
 }
 
 export default PageAddFoodToLibrary;
